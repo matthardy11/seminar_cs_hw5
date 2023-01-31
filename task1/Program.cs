41bab4e46e119f52d6e31aefabe636452043b0af
matthardy11 <matthardy2560@gmail.com>
2023-01-31 19:21:32 +0400

    comlete task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] FillRandomArray(int arrSize)
+{
+  Random rand = new Random();
+  int[] array = new int[arrSize];
+  for(int i = 0; i < array.Length; i++)
+  {
+    array[i] = rand.Next(100, 1000);
+  }
+  return array;
+}
+
+void FindPositive(int[] array)
+{
+  int count = 0;
+  for(int i = 0; i < array.Length; i++)
+  {
+    if(array[i] % 2 == 0)
+    {
+      count++;
+    }
+  }
+  System.Console.WriteLine("Колличество положительных чисел в массиве: " + count);
+}
+
+FindPositive(FillRandomArray(30));
